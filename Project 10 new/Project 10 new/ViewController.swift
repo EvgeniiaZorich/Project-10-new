659a91aaad633fde265d9f7c14d1cc65c121f319
Evgeniia Zorich <vasileva.evg.al@gmail.com>
2023-01-24 18:57:36 +0100

    initial commit

---
+++ Project 10 new/Project 10 new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 10 new
+//
+//  Created by Евгения Зорич on 24.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
